e3d5b4315e651f7e74cd3127660f0478ea84cf57
srz-vishnu <vishnuk@smartrabbitz.com>
2025-07-16 13:25:26 +0530

    product page intergartion with api

--- src/api/Api.js
+++ src/api/Api.js
@@ -18,7 +18,23 @@ const getAllBranchDetails = async() => {
     }
 }
 
+const getBrandDetailsById = async(id) => {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(`${base_URL}/product/brand/${id}`,{
+            headers : {
+                Authorization: `Bearer ${token}` 
+            }
+        });
+        const data = response?.data?.result || [];
+        return data;
+    } catch (error) {
+        console.log(error,'get all brand details for product page');
+        
+    }
+}
+
 
 export {
-    getAllBranchDetails
+    getAllBranchDetails, getBrandDetailsById
 }
